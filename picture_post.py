f5622f6adae0c3a82493449ee415b4501e6b91d4
avlon13 <wushiqing0@gamil.com>
2021-09-10 21:03:15 +0900

    画像取得・送信プログラムの合併

--- picture_post.py
+++ picture_post.py
@@ -1,16 +1,47 @@
+import cv2
+from datetime import datetime
+import os
+import time
 import base64
 import requests
 import json
 
-url = 'http://127.0.0.1:8000/photo/in/'
+#撮影処理
+def picGet(c_num):
+    date = datetime.now().strftime("%Y%m%d_%H%M%S")
+    dir_path = './' + date + '/'
+    os.makedirs(dir_path)
 
-f = open('1.png', 'rb')
-#base64
-base64_data = base64.b64encode(f.read())
-f.close()
-base64_data = base64_data.decode()
+    cap = cv2.VideoCapture(0)
+    count = 0
+    time.sleep(0.3)
 
-data = {'img':base64_data}
 
-r = requests.post(url,headers = {'Content-Type':'application/json',},data =json.dumps(data))
-print(r.text)
+    #撮影ループ
+    while True:
+        if count >= c_num:
+            break
+
+        ret, frame = cap.read()
+        path = dir_path + str(count) + ".png"
+        cv2.imwrite(path, frame)
+        url = 'http://127.0.0.1:8000/photo/in/'
+
+        f = open(path, 'rb')
+        # base64编码
+        base64_data = base64.b64encode(f.read())
+        f.close()
+        base64_data = base64_data.decode()
+        # 传输的数据格式
+        data = {'img': base64_data}
+
+        r = requests.post(url, headers={'Content-Type': 'application/json', }, data=json.dumps(data))
+        print(r.text)
+        time.sleep(0.2)
+        count += 1
+
+    #開放
+    cap.release()
+    cv2.destroyAllWindows()
+
+picGet(10)
