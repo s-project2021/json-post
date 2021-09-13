f8206616476f9576853eb42327996802dac8a3d1
avlon13 <84111234+avlon13@users.noreply.github.com>
2021-09-13 15:35:00 +0900

    naiveの時間データをawareに変換

--- ai_json_post.py
+++ ai_json_post.py
@@ -1,11 +1,15 @@
 import requests
 import json
 from datetime import date,datetime
+from pytz import timezone
 
 url = "http://127.0.0.1:8000/ai/"
 
+now_time = datetime.now()
+timez = timezone('Asia/Tokyo')
+then_time = timez.localize(now_time)
 
-data = {"place_id":"1","then_time":datetime.now(),"crowd":"4"}
+data = {"place_id":"1","then_time":then_time,"crowd":"4"}
 
 def json_serial(obj):
 
